b91e18c53046a42446d8e617a0ff25d1a579d65e
SmileSRT <korovichev367@gmail.com>
2025-01-18 18:14:58 +0300

    feat(heart-icon): add heart-icon and handlers for like image

--- src/pages/home/ui/cat-image-list.tsx
+++ src/pages/home/ui/cat-image-list.tsx
@@ -1,12 +1,28 @@
-import type { FC } from "react";
+import { useState, type FC } from "react";
 import type { ICatItem } from "../api/cats.service";
 import styles from "./image.module.css";
+import HeartIcon from "./heart-button";
+
+const HeartButton: FC = () => {
+  const [isLiked, setIsLiked] = useState<boolean>(false);
+  const activeStyle = [styles.heart__button, styles.active].join(" ");
+
+  return (
+    <button
+      className={isLiked ? activeStyle : styles.heart__button}
+      onClick={() => setIsLiked(!isLiked)}
+    >
+      <HeartIcon isLiked={isLiked} />
+    </button>
+  );
+};
 
 const CatImageTile: FC<ICatItem> = ({ url }) => {
   return (
-    <>
-      <img width={255} height={255} src={url} className={styles.image__tile} />
-    </>
+    <div className={styles.tile__container}>
+      <img width={225} height={225} src={url} className={styles.image__tile} />
+      <HeartButton />
+    </div>
   );
 };
 
