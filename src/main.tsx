7b7ab83cf205332bd5b5ced8bbbcfa797ac215d8
SmileSRT <korovichev367@gmail.com>
2025-01-20 02:15:02 +0300

    fix(router): change router on hash-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app/app.tsx";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 import RootLayout from "./shared/ui/root-layout.tsx";
 import "./shared/ui/styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <RootLayout>
         <App />
       </RootLayout>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
